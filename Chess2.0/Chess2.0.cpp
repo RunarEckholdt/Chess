00953e95c395413d087dd94bcb47cbd0f08b14fd
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2020-01-18 00:53:24 +0100

    making the chess pieces and added a enum to string function

--- Chess2.0/Chess2.0.cpp
+++ Chess2.0/Chess2.0.cpp
@@ -22,6 +22,16 @@ public:
 		xPos = x;
 		yPos = y;
 	}
+
+	vector <int> getPos() {
+		vector <int> pos;
+		pos.push_back(xPos);
+		pos.push_back(yPos);
+		return pos;
+	}
+	
+
+	
 	
 	Piece(Kind k, Color c, int i, int x, int y) {
 		kind = k;
@@ -36,7 +46,7 @@ public:
 
 
 
-class PieceOverwiev {
+class PieceOverview {
 	Kind kind;
 	Color color;
 	int piecesLeft;
@@ -49,7 +59,7 @@ public:
 		piecesLeft--;
 	}
 
-	PieceOverwiev(Kind k, Color c) {
+	PieceOverview(Kind k, Color c) {
 		kind = k;
 		color = c;
 		switch (kind)
@@ -80,13 +90,101 @@ public:
 };
 
 vector <Piece> pieces;
-vector <PieceOverwiev> pieceOverwiev;
+vector <PieceOverview> pieceOverview;
 
 void makePieces() {
-	for(int i = 0; i < )
+	int id = 1;
+	Color c = White;
+	for (int y = 0; y < 2; y++) {
+			for (int i = 1; i <= 8; i++) {//making pawns first round white, second round black
+				Piece piece(Pawn, c, id, i, 2+5*y);
+				pieces.push_back(piece);
+				id++;
+			}
+			c = Black;
+			id = 17;
+		}
+	id = 9;
+	c = White;
+	for (int y = 0; y < 2; y++) {
+		for (int i = 0; i < 2; i++) {//making rooks
+			Piece piece(Rook, c, id, 1+7*i, 1 + 7 * y);
+			pieces.push_back(piece);
+			id++;
+		}
+		id = 25;
+		c = Black;
+	}
+	id = 11;
+	c = White;
+	for (int y = 0; y < 2; y++) {
+		for (int i = 0; i < 2; i++) {//making knights
+			Piece piece(Knight, c, id, 2 + 5 * i, 1 + 7 * y);
+			pieces.push_back(piece);
+			id++;
+		}
+		id = 27;
+		c = Black;
+	}
+
+	id = 13;
+	c = White;
+	for (int y = 0; y < 2; y++) {
+		for (int i = 0; i < 2; i++) {//making bishop
+			Piece piece(Bishop, c, id, 3 + 3 * i, 1 + 7 * y);
+			pieces.push_back(piece);
+			id++;
+		}
+		id = 29;
+		c = Black;
+	}
+	id = 15;
+	c = White;
+	for (int y = 0; y < 2; y++) {//making queens
+		Piece piece(Queen, c, id, 4, 1 + 7 * y);
+		pieces.push_back(piece);
+		id = 31;
+		c = Black;
+	}
+	id = 16;
+	c = White;
+	for (int y = 0; y < 2; y++) {//making kings
+		Piece piece(King, c, id, 5, 1 + 7 * y);
+		pieces.push_back(piece);
+		id = 32;
+		c = Black;
+	}
+
+
+
+	
 }
 
-
+string stringKind(Kind kind) {
+	switch (kind)
+	{
+	case Pawn:
+		return "Pawn";
+		break;
+	case Rook:
+		return "Rook";
+		break;
+	case Knight:
+		return "Knight";
+		break;
+	case Bishop:
+		return "Bishop";
+		break;
+	case Queen:
+		return "Queen";
+		break;
+	case King:
+		return "King";
+		break;
+	default:
+		break;
+	}
+}
 
 
 
