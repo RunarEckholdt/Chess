ce3eda583b9b973a7c5df09763398ff28371d14a
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2020-01-17 19:54:28 +0100

    Classes

--- Chess2.0/Chess2.0.cpp
+++ Chess2.0/Chess2.0.cpp
@@ -1,20 +1,101 @@
-// Chess2.0.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "../std_lib_facilities.h"
+enum Kind {Pawn,Rook,Knight,Bishop,Queen,King};
+enum Color {White,Black};
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+class Piece {
+	Kind kind;
+	Color color;
+	int id;
+	int xPos;
+	int yPos;
+public:
+	Kind getKind() { return kind; }
+	Color getColor() { return color; }
+	int getId() { return id; }
+	int getXPos() { return xPos; }
+	int getYPos() { return yPos; }
+
+	void changePos(int x, int y) {
+		xPos = x;
+		yPos = y;
+	}
+	
+	Piece(Kind k, Color c, int i, int x, int y) {
+		kind = k;
+		color = c;
+		id = i;
+		xPos = x;
+		yPos = y;
+	}
+
+
+};
+
+
+
+class PieceOverwiev {
+	Kind kind;
+	Color color;
+	int piecesLeft;
+public:
+	Kind getKind() { return kind; }
+	Color getColor() { return color; }
+	int getPiecesLeft() { return piecesLeft; }
+
+	void pieceDead() {
+		piecesLeft--;
+	}
+
+	PieceOverwiev(Kind k, Color c) {
+		kind = k;
+		color = c;
+		switch (kind)
+		{
+		case Pawn:
+			piecesLeft = 8;
+			break;
+		case Rook:
+			piecesLeft = 2;
+			break;
+		case Knight:
+			piecesLeft = 2;
+			break;
+		case Bishop:
+			piecesLeft = 2;
+			break;
+		case Queen:
+			piecesLeft = 1;
+			break;
+		case King:
+			piecesLeft = 1;
+			break;
+		default:
+			break;
+		}
+	}
+
+};
+
+vector <Piece> pieces;
+vector <PieceOverwiev> pieceOverwiev;
+
+void makePieces() {
+	for(int i = 0; i < )
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+
+
+
+int main() {
+
+
+
+	return 0;
+}
